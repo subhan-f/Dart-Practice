10bf7d66daf7f09ca83d3bbd24bbb975bcef07fb
Subhan F. <mahersubhan578@gmail.com>
2024-05-19 18:47:03 +0500

    var

--- 10_.dart
+++
@@ -1,5 +0,0 @@
-// This is a Dart file
-// Filename: 10_.dart
-void main() {
-  print("Hello from file {i}!");
-}
---
+++ 10_var.dart
@@ -0,0 +1,8 @@
+void main() {
+  // we can store any data type of data in var variable (dynamic)
+  var data1, data2, data3, data4;
+  data1 = 876;
+  data2 = 9.9;
+  data3 = [8, 9, 9];
+  data4 = "lorem ipsem";
+}
