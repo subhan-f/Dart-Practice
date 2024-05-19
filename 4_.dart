c3aa9a9cf0559e6e702898dfdbfaa08f39fe395e
Subhan F. <mahersubhan578@gmail.com>
2024-05-19 17:39:58 +0500

    Data Types

--- 4_.dart
+++
@@ -1,5 +0,0 @@
-// This is a Dart file
-// Filename: 4_.dart
-void main() {
-  print("Hello from file {i}!");
-}
---
+++ 4_dataTypes.dart
@@ -0,0 +1,11 @@
+void main() {
+  int num = 1; // integer
+  double x = 3.4; // double or decimal
+  bool y = true; // Boolean (True or False)
+  String name = 'Subhan'; // String
+  
+  print("num = $num");
+  print("x = $x");
+  print("y = $y");
+  print("name = $name");
+}
