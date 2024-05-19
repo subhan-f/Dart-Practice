13c21d8694d47e9deade0e8ba87b11e8c9578b7e
Subhan F. <mahersubhan578@gmail.com>
2024-05-19 17:31:42 +0500

    variables

---
+++ 3_variables.dart
@@ -0,0 +1,14 @@
+// This is a Dart file
+// Filename: 3_.dart
+import 'dart:ffi';
+
+void main() {
+  int variable1; // declaration
+  variable1 = 1; // initialization
+  variable1 = 2; // changing variable
+  
+  double variable2 = 2.2;
+
+  print("Variable1 = $variable1");
+  print("Variable2 = $variable2");
+}
