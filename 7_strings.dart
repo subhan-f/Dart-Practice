658ad14aa4dccd0a89d8240d6f90b162c16fbed6
Subhan F. <mahersubhan578@gmail.com>
2024-05-19 18:30:05 +0500

    More with strings

--- 7_strings.dart
+++ 7_strings.dart
@@ -5,5 +5,10 @@ void main() {
   print(name);
   print("Index 0 of name is " + name[0]);
   print("Length of name is " + name.length.toString());
-
+  print("Name in upper case: " + name.toUpperCase());
+  print("Name in lower case: " + name.toLowerCase());
+  print("Split: " + name.split('').toString());
+  print("Replace: " + name.replaceAll('S', '5'));
+  print("Name contains a: " + name.contains('a').toString());
+  
 }
