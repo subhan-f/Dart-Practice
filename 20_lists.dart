ddb6ea5442d58756a8d34d0dfa634a9086e1feac
Subhan F. <mahersubhan578@gmail.com>
2024-05-20 16:39:41 +0500

    list

---
+++ 20_lists.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  List<String> names = ["Subhan", "Ali"];
+  List<int> ages = [21, 12];
+  for (var i = 0; i < ages.length; i++) {
+    print("Age: ${ages[i]}");
+  }
+  for (var i = 0; i < names.length; i++) {
+    print("Age: ${names[i]}");
+  }
+}
